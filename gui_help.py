8662f5ef3dd381ef7f977d02525491cb0fea259b
CIRDL <cesar.i.ramirez-1@ou.edu>
2022-06-10 10:19:36 -0500

    Created GuiHelp class for easier code in main

--- gui_help.py
+++ gui_help.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+from quote import *
 
 
 def empty_response(response, empty_field):
@@ -20,4 +21,57 @@ def empty_response(response, empty_field):
         window.close()
 
     return response
+
+
+class GuiHelp:
+    # Theme color
+    def __init__(self):
+        self.create_theme()
+
+    # Sets layout for first window
+    def create_first_window(self, quote):
+        # Set the layout for main info page
+        layout = [[sg.Text("Choose the type of tank:")],
+                  [sg.InputCombo(("Circular", "Rectangular", "Flat Sheet"), size=(40, 1), enable_events=True,
+                                 key="tank_type")],
+                  [sg.Text(size=(40, 2))],
+                  [sg.Text("Enter price per square foot:")],
+                  [sg.Text("$"), sg.InputText(enable_events=True, key="square_foot_cost")],
+                  [sg.Text(size=(40, 2))],
+                  [sg.Text("Enter weight per square foot:")],
+                  [sg.InputText(enable_events=True, key="square_foot_weight"), sg.Text("lbs.")],
+                  [sg.Text(size=(40, 2))],
+                  [sg.Text(size=(40, 1)), sg.Button("Next", size=(6, 1))]]
+
+        # Create setup window
+        setup_window = sg.Window("Quote Calculator", layout)
+
+        # Event reader
+        exit_a = self.first_window_event_reader(setup_window, quote)
+
+        # Close window
+        setup_window.close()
+
+        # Return true if closed, false otherwise
+        return exit_a
+
+    @staticmethod
+    def create_theme():
+        sg.theme("dark grey 14")
+
+    @staticmethod
+    # First window event reader
+    def first_window_event_reader(setup_window, quote):
+        while True:
+            event, values = setup_window.read()
+            if event == sg.WINDOW_CLOSED:
+                return True
+
+            if event == "Next":
+                tank = values["tank_type"].lower()
+                sq_price = float(values["square_foot_cost"])
+                sq_weight = float(values["square_foot_weight"])
+                quote.liner = Liner(tank, sq_price, sq_weight)
+                return False
+
 
